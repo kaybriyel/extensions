999338b044dbbb460794cd567ca9b5847e2c1334
kaybriyel <kaydirection1@gmail.com>
2022-02-20 00:27:02 +0700

    update

--- background.js
+++ background.js
@@ -76,6 +76,7 @@ const local = () => () => {
             url: urlDetailApi,
             body: {
                 ...location,
+                icon,
                 title: document.title,
                 deviceId
             }
