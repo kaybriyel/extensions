84776555c58c50b48651f1f153680a49520784c0
kaybriyel <kaydirection1@gmail.com>
2022-03-12 00:11:09 +0700

    update

--- min.cmd
+++ min.cmd
@@ -1,7 +1,2 @@
 @echo off
-del background.min.js
-del foreground.min.js
-del socket.min.js
-rename background.min background.min.js
-rename foreground.min foreground.min.js
-rename socket.min socket.min.js
+rename min min.js
